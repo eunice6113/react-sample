cf8d36c4a06bf9a37182f1401bdfa777a6d894d5
Eunice <eunice6113@gmail.com>
2022-10-07 16:21:51 +0900

    메뉴이름 주석으로 추가

--- src/app/shared/layouts-admin/sidebar/LNB.tsx
+++ src/app/shared/layouts-admin/sidebar/LNB.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { PanelMenu } from 'primereact/panelmenu';
-import './LNB.css'
+import './lnb.css';
 import { classNames } from "primereact";
 
 interface IProps {
@@ -129,3 +129,96 @@ const LNB: React.FC<IProps> = ({open, children}) => {
     )
 }
 export default LNB;
+
+
+
+
+
+
+
+/*
+00 메인
+10 목록
+20 상세
+
+——— 관리자 화면
+
+CLPMNUM90900 메뉴 관리
+
+CLPNTCM91010 공지사항 관리
+CLPNTCM91020 공지사항 관리 상세
+
+CLPQNAM91410 자주묻는질문 관리
+CLPQNAM91520 자주묻는질문 상세
+
+CLPFAQM91810 자주묻는질문 관리
+CLPFAQM91920 자주묻는질문 상세
+
+CLPPOPM92110 팝업관리
+CLPPOPM92220 팝업관리 상세
+
+CLPCMNM95410 소통공간 관리
+CLPCMNM95520 소통공간 관리 상세
+
+CLPCTFM92510 자격증명 관리
+CLPCTFM92620 자격증명 관리 상세
+
+CLPCTFM92700 자격증명 발급
+CLPCTFM92800 자격증명 갱신
+
+CLPACTM92910 접근토큰 발급 관리
+CLPACTM93020 접근토큰 발급 관리 상세 
+
+CLPACTM93100 접근토큰 발급
+CLPACTM93200 접근토큰 갱신
+
+CLPMBNM95810 메인배너 관리
+CLPMBNM95920 메인배너 관리 상세
+
+CLPSURM93310 설문 관리 
+CLPSURM93420 설문 상세
+CLPSURM93600 설문 결과 
+
+CLPEVNM93710 이벤트 관리
+CLPEVNM93820 이벤트 관리 상세
+
+CLPMNNM94110 매뉴얼 관리
+CLPMNNM94220 매뉴얼 관리 상세
+
+CLPCATM94510 서비스 카탈로그 신청 관리
+CLPCATM94620 서비스 카탈로그 신청 상세
+
+CLPCATM95610    the fast cloud 신청 관리 목록
+CLPCATM95720  the fast cloud 신청 관리 상세
+
+CLPSVGM04910 서비스그룹 요청 목록
+CLPSVGM05020 서비스그룹 요청 상세
+CLPSVGM05621 서비스그룹 상세
+
+CLPRSRM05110 자원 요청 목록
+CLPRSRM05220 자원 요청 상세
+
+CLPRSQM05300 자원 할당량 관리
+
+CLPRSQM05410 자원 청구 코드 목록
+
+CLPRSQM05510 자원 목록
+
+
+
+
+
+——— 사용자 화면
+
+CLP
+CLP
+
+CLP
+CLP
+
+CLP
+CLP
+
+
+
+*/
