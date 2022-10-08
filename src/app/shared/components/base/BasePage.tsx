440f842214799854416184e702317f117fc0356a
eunice6113 <eunice6113@gmail.com>
2022-10-08 21:39:05 +0900

    페이지타이틀

--- src/app/shared/components/base/BasePage.tsx
+++ src/app/shared/components/base/BasePage.tsx
@@ -19,28 +19,39 @@ export const BasePage: React.FC<IProps> = ({children}) => {
         curLocation[3] = ':id'
     }
 
-    routes.map((item, index) => {
-        item.children?.map((sub:any, sidx) => {
-            sub.children?.map((tir:any, tidx:any) => {
-                tir.children?.map((four:any, fidx:any) => {
-                    console.log(curLocation.length, sub.path, tir.path, tir.name, four.path, four.name)
-                    
-                    if(curLocation.length === 3 &&
-                        curLocation[1] === sub.path && curLocation[2] === tir.path) 
-                     {
-                        //  console.log(' ==>', tir.name)
-                         pageTitle = tir.name;
-                     }
-                     else if(curLocation.length === 4 &&
-                         curLocation[1] === sub.path && curLocation[2] === tir.path && curLocation[3] === four.path) 
-                      {
-                        //   console.log(' ==>', four.name)
-                          pageTitle = four.name;
-                      }
-                })
+    //이 방법 말고 path + children path 를 / 로 flatten 해서 읽어오게 개선하기
+    //breadcrumb 도 개발해야함
+    routes.map((item) => {
+        item.children?.map((sub:any) => {
+            sub.children?.map((tir:any) => {
+                if(curLocation[1] === sub.path && curLocation[2] === tir.path) {
+                    if(curLocation.length === 3) {
+                        pageTitle = tir.name;
+                        return;
+                   } else {
+                        tir.children?.map((four:any) => {
+                            // console.log(curLocation.length, sub.path, tir.path, tir.name, four.path, four.name)
+                            if(curLocation.length === 4 && curLocation[3] === four.path) {
+                                pageTitle = four.name;
+                            }
+                        })
+                   }
+                } 
             })
         })
     })
+
+    // function getFlat(array:any, n = '', p = '') {
+    //     return array.reduce((r:any, { children = [], ...o }) => {
+    //         var name = n + (n && '.') + o.name,
+    //             path = p + o.path;
+    //         return r.concat(Object.assign({}, o, { name, path }), getFlat(children, name, path));
+    //     }, []);
+    // }
+    
+    // let flat = getFlat([routes]);
+    // console.log(flat);
+
 
     //새로운 곳으로 이동시 페이지 상단으로 스크롤 이동
     React.useEffect(() => {
