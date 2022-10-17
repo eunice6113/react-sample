e004566039355e8fd07b48c289991d05396f2df9
Eunice <eunice6113@gmail.com>
2022-10-17 13:36:14 +0900

    메뉴관리

--- src/app/pages/ui-guide/InputGuide.tsx
+++ src/app/pages/ui-guide/InputGuide.tsx
@@ -1,4 +1,4 @@
-import { InputNumber, InputText } from "primereact";
+import { InputNumber, InputText, InputTextarea } from "primereact";
 import * as React from "react";
 import { BasePage } from "../../shared/components/base/BasePage";
 import TextEditor from "../../shared/components/ui/text-editor/TextEditor";
@@ -16,12 +16,14 @@ const InputGuide: React.FC = () => {
         <h3>Basic</h3>
         <InputText className="" placeholder="제목을 입력해주세요" value={title} onChange={(e) => setTitle(e.target.value)} />
 
+        <h3>Textarea</h3>
+        <InputTextarea value={content} onChange={(e) => setContent(e.target.value)} rows={5} cols={30} />
+
         <h3>숫자</h3>
         <InputNumber className='orderNm' inputId="integeronly" value={order} onValueChange={(e) => setOrder(e.value)} />
     
         <h3>텍스트 에디터</h3>
         <TextEditor value={content} onTextChange={setContent} />
-
         
     </BasePage>)
 }
