83d5ff4f3a008baac936f8a2bd4548f17b6720d6
Eunice <eunice6113@gmail.com>
2022-11-11 18:09:41 +0900

    breadcrumb 수정

--- src/app/shared/components/page-title/PageTitle.tsx
+++ src/app/shared/components/page-title/PageTitle.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Menubar } from 'primereact/menubar';
 import './page-title.css'
 import { Link } from 'react-router-dom';
+import BreadCrumbs from '../breadcrumbs/BreadCrumbs';
 
 interface IProps {
     title: string;
@@ -14,78 +15,12 @@ const PageTitle: React.FC<IProps> = ({title, breadcrumbs}) => {
 
     }, [])
 
-    const items = [
-        {
-            label: 'menu1',
-            items: [
-                {
-                    label: 'Left',
-                    url: '',
-                },
-                {
-                    label: 'Right',
-                    url: '',
-                },
-                {
-                    label: 'Center',
-                    url: '',
-                },
-                {
-                    label: 'Justify',
-                    url: '',
-                },
-
-            ]
-        },
-    ];
-
-    const items2 = [
-        {
-            label: 'menu2',
-            items: [
-                {
-                    label: 'Left',
-                    url: '',
-                },
-                {
-                    label: 'Right',
-                    url: '',
-                },
-
-            ]
-        },
-    ];
-
-    const items3 = [
-        {
-            label: 'menu3',
-            items: [
-                {
-                    label: 'Left',
-                    url: '',
-                },
-                {
-                    label: 'Right',
-                    url: '',
-                },
-
-            ]
-        },
-    ];
 
     return(
     <div className='pageTitleContainer'>
         <h1>{title}</h1>
 
-        <div className='breadCrumbs'>
-            <Link to='/' className='menuTxt'>Home</Link> 
-            <i className='pi pi-chevron-right' />
-            <Menubar model={items} />
-            <i className='pi pi-chevron-right' />
-            <Menubar model={items2} />
-            <i className='pi pi-chevron-right' />
-            <Menubar model={items3} />
-        </div>
+        <BreadCrumbs />
     </div>
     )
 }
